2cd7c3d73215fb5b00caaedfcf334d6d052dc21c
yuki <miyatayuki1113@keio.jp>
2025-01-06 20:06:54 +0900

    add: FrenetPathクラスを作成

--- src/artery/application/mcs/FrenetPath.cc
+++ src/artery/application/mcs/FrenetPath.cc
@@ -1,9 +1,50 @@
 
+#include "QuarticPolynomial.h"
+#include "QuinticPolynomial.h"
+#include "Trajectory.h"
 #include "FrenetPath.h"
 #include <vector>
+#include <iostream>
 
 namespace artery
 {
 
-FrenetPath::generateFrenetPath(
+void FrenetPath::calculateCost() {
+    double latJerkSquareSum = 0.0;
+    double lonJerkSquareSum = 0.0;
+    double latDiffSpeed = mLatTrajectory.getTargetSpeed() - mLatTrajectory.getSpeeds().back();
+    double latCost;
+    double lonCost;
+
+    for (int jerk: mLatTrajectory.getJerks()) {
+        latJerkSquareSum += jerk * jerk;
+    }
+
+    for (int jerk: mLonTrajectory.getJerks()) {
+        lonJerkSquareSum += jerk * jerk;
+    }
+
+    // 縦方向のコスト = ジャークの二乗和 + 収束時間 + 目標スピードとの差分自乗
+    latCost = K_JERK * latJerkSquareSum + 
+              K_TIME * mLatTrajectory.getConvergenceTime() + 
+              K_SPEED * latDiffSpeed * latDiffSpeed;
+    // 横方向のコスト = ジャークの二乗和 + 収束時間 + センターラインまでの距離の二乗
+    lonCost = K_JERK * lonJerkSquareSum + 
+              K_TIME * mLonTrajectory.getConvergenceTime() + 
+              K_DISTANCE * mLonTrajectory.getPoses().back() * mLonTrajectory.getPoses().back(); 
+    mCost = K_LAT * latCost + K_LON * lonCost;
 }
+}
+
+// ユニットテスト
+// 実行方法
+// mcsディレクトリ下で $g++ -DFRENETPATH_TEST FrenetPath.cc Trajectory.cc QuinticPolynomial.cc QuarticPolynomial.cc
+#ifdef FRENETPATH_TEST
+int main() {
+    artery::QuinticPolynomial qp1(0.0, 0.0, 0.0, 10.0, 0.0, 0.0, 5.0);
+    artery::QuarticPolynomial qp2(0.0, 0.0, 0.0, 2.0, 0.0, 5.0);
+    artery::FrenetPath fp(qp2, qp1);
+    fp.calculateCost();
+    std::cout << "Cost: " << fp.getCost() << std::endl;
+}
+#endif
--- src/artery/application/mcs/FrenetPath.h
+++ src/artery/application/mcs/FrenetPath.h
@@ -1,30 +1,42 @@
 #ifndef ARTERY_FRENETPATH_H
 #define ARTERY_FRENETPATH_H
 
+#include "Trajectory.h" 
 #include <vector>
-
 namespace artery
 {
 
 class FrenetPath
 {
 public:
+	// デフォルトコンストラクタ
+	FrenetPath(const QuarticPolynomial& lonPolynomial, const QuinticPolynomial& latPolynomial) {
+		mLonTrajectory = Trajectory(lonPolynomial);
+		mLatTrajectory = Trajectory(latPolynomial);
+	}
 
-	// getter
-	const mlonPath& getLonPath { return mLonPath; }
-	const mLatPath& getLatPath { return mLatPath; }
+	static constexpr double K_JERK = 0.1; // ジャークの重み
+	static constexpr double K_TIME = 1.0; // 時間の重み
+	static constexpr double K_DISTANCE = 3.0; // 距離の重み
+	static constexpr double K_SPEED = 3.0; // 速度の重み
+	static constexpr double K_LAT = 1.0; // 横方向の重み
+	static constexpr double K_LON = 1.0; // 縦方向の重み
+
+	// getter 
+	const artery::Trajectory& getLonTrajectory() { return mLonTrajectory; }
+	const artery::Trajectory& getLatTrajectory() { return mLatTrajectory; }
 	double getCost() { return mCost; }
 
 	// setter
-	void setLonPath(const Path& lonPath) { mLonPath = lonPath; }
-	void setLatPath(const Path& latPath) { mLatPath = latPath; }
+	void setLonTrajectory(const artery::Trajectory& lonTrajectory) { mLonTrajectory = lonTrajectory; }
+	void setLatTrajectory(const artery::Trajectory& latTrajectory) { mLatTrajectory = latTrajectory; }
 
 	// 経路のコストを計算
 	void calculateCost();
 
 private:
-	Path mlonPath;　// 縦方向のパス
-	Path mlatPath; // 横方向のパス
+	Trajectory mLonTrajectory; // 縦方向のパス
+	Trajectory mLatTrajectory; // 横方向のパス
 	double mCost; // 経路のコスト
 };
 }
